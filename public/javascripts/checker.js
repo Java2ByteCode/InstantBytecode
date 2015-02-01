c4bb41471441da921da9c2dc9515e662619fef5b
yen_lee <i745m@hotmail.com>
2015-02-01 12:52:22 +0800

    Apply CodeMirror to output textarea.

--- public/javascripts/checker.js
+++ public/javascripts/checker.js
@@ -2,18 +2,22 @@ var socket = io.connect('/');
 var waitMilliSec = 1000;
 var oldJavaCode = '';
 var oldClassName = '';
-
+var outputBcCM;
 
 socket.on('byte_code', function(data) {
-	$('#output_bc').val(data.code);
+	outputBcCM.setValue(data.code);
 });
 
 socket.on('wrong', function(data) {
-	$('#output_bc').val(data.err);
+	outputBcCM.setValue(data.err);
 });
 
 $(function() {
 	var inputJavaCM = CodeMirror.fromTextArea(document.getElementById('input_java'), {
+		mode: 'clike',
+		lineNumbers: true
+	});
+	outputBcCM = CodeMirror.fromTextArea(document.getElementById('output_bc'), {
 		mode: 'clike',
 		lineNumbers: true
 	});
