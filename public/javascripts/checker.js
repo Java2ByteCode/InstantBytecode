6fc6444df6cf8fb77b3e521f63b1d52afd7c5168
yen_lee <i745m@hotmail.com>
2015-02-01 09:10:33 +0800

    Simplify the way we determine when to upload java code.

--- public/javascripts/checker.js
+++ public/javascripts/checker.js
@@ -1,5 +1,7 @@
 var socket = io.connect('/'); 
-var idleSec = 0;
+var waitMilliSec = 1000;
+var oldJavaCode = '';
+var oldClassName = '';
 
 socket.on('byte_code', function(data) {
 	$('#output_bc').val(data.code);
@@ -10,47 +12,20 @@ socket.on('wrong', function(data) {
 });
 
 $(function() {
-	var waitMilliSec = 1000;
-	var idleIntervalId = setInterval(tick, waitMilliSec);
-	var codeSentTimeStamp = new Date().getTime() - waitMilliSec;
-	//console.log('codeSentTimeStamp: ' + codeSentTimeStamp);
-	$('#input_java').bind('input propertychange', function() {
-		var now = new Date().getTime();
-		var className = $('#class_name').val();
-		//console.log('now: ' + now);
-		//console.log('diff: ' + (now - codeSentTimeStamp));
-		console.log('className not empty : ' + (className !== ''));
-		console.log('class Name: ' + className);
-		if (now - codeSentTimeStamp >= waitMilliSec && className !== '') {
-			console.log('upload from top');
+	var idleIntervalId = setInterval(function() {
+		var newJavaCode = $('#input_java').val();
+		var newClassName = $('#class_name').val();
+		checkDiff(newJavaCode, newClassName);
+	} , waitMilliSec);
+
+	function checkDiff(newJavaCode, newClassName) {
+		if((newJavaCode != oldJavaCode) || (newClassName != oldClassName)) {
 			socket.emit('code_sent', {
 				code: $('#input_java').val(),
-				className: className
+				className: $('#class_name').val()
 			});	
-			codeSentTimeStamp = new Date().getTime();
-			idleIntervalId = setInterval(tick, waitMilliSec);
-		} 
-	});
-
-	$(this).mousemove(function (e) {
-        idleSec = 0;
-    });
-    $(this).keypress(function (e) {
-        idleSec = 0;
-    });
-	
-	function tick() {
-		console.log('tick: ' + idleSec);
-		var className = $('#class_name').val();
-    	idleSec = idleSec + 1;
-    	if (idleSec >= 1 && className !== '') {
-    		// if idle for more than 1 second upload the code
-    		console.log('upload from tick()');
-        	socket.emit('code_sent', {
-				code: $('#input_java').val(),
-				className: className
-			});
-			clearInterval(idleIntervalId);
-    	}
+			oldJavaCode = newJavaCode;
+			oldClassName = newClassName;
+		}
 	}
 });
