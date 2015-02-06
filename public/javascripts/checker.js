ca6d6bda373a33cc1c11e07a7feed0eb7582db90
yen_lee <i745m@hotmail.com>
2015-02-06 21:22:18 +0800

    Upload the code only when the code doesn't change for 1 second.

--- public/javascripts/checker.js
+++ public/javascripts/checker.js
@@ -1,9 +1,8 @@
 var socket = io.connect('/'); 
-var waitMilliSec = 1000;
-var oldJavaCode = '';
-var oldClassName = '';
+var delayMilliSec = 1000;
 var inputJavaCM;
 var outputBcCM;
+var codeUploader;
 
 socket.on('byte_code', function(data) {
 	outputBcCM.setValue(data.code);
@@ -13,34 +12,36 @@ socket.on('wrong', function(data) {
 	outputBcCM.setValue(data.err);
 });
 
+function delayedUpload() {
+	clearTimeout(codeUploader);
+	codeUploader = setTimeout(function() {
+		socket.emit('code_sent', {
+			code: inputJavaCM.getValue(),
+			className: $('#class_name').val()
+		});	
+	}, delayMilliSec);	
+}
+
 $(function() {
+	
+	$('#class_name').bind('input propertychange', function(){
+		delayedUpload();
+	});
+
 	inputJavaCM = CodeMirror.fromTextArea(document.getElementById('input_java'), {
 		styleActiveLine: true,
 		mode: 'clike',
 		lineNumbers: true
 	});
+	inputJavaCM.on('change', function(cMirror) {
+		delayedUpload();
+	});
 	inputJavaCM.setSize("100%", "100%");
-	
+
 	outputBcCM = CodeMirror.fromTextArea(document.getElementById('output_bc'), {
 		mode: 'clike',
 		lineNumbers: true
 	});
 	outputBcCM.setSize("100%", "100%");
 
-	var idleIntervalId = setInterval(function() {
-		newJavaCode = inputJavaCM.getValue();
-		newClassName = $('#class_name').val();
-		checkDiff(newJavaCode, newClassName);
-	} , waitMilliSec);
-
-	function checkDiff(newJavaCode, newClassName) {
-		if((newJavaCode != oldJavaCode) || (newClassName != oldClassName)) {
-			socket.emit('code_sent', {
-				code: newJavaCode,
-				className: newClassName
-			});	
-			oldJavaCode = newJavaCode;
-			oldClassName = newClassName;	
-		}
-	}
 });
